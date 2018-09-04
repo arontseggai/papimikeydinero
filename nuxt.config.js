ec7cb44e85dec1663801431d80990755b83fcc4a
arontseggai <aron.tseggai@gmail.com>
2018-09-04 13:57:42 +0200

    Oops type error images on gh pages hosting should work after this change

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,7 @@ module.exports = {
   ],
   // router: { base: '/papimikeydinero/' },
   router: { 
-    ...(process.env.NODE_ENV !== 'PRODUCTION' ? {base: ''} : {base: '/papimikeydiner/'})
+    ...(process.env.NODE_ENV !== 'PRODUCTION' ? {base: ''} : {base: '/papimikeydinero/'})
    },
   /*
   ** Build configuration
