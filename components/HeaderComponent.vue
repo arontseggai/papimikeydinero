b4f58ed61dc6daf8e20be64754e37aecff77ce4b
arontseggai <aron.tseggai@gmail.com>
2018-09-17 13:07:14 +0200

    Adjusts header component to fit for papimikeydinero

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -1,9 +1,22 @@
 <template>
     <header>
-        <img src="~/assets/img/mike_single_cover_compressed.png" alt="Header image Mikey Dinero Single Cover">
+        <img class="header-image" src="~/assets/img/mike_single_cover_compressed.png" alt="Header image Mikey Dinero Single Cover">
     </header>
 </template>
 
 <script>
 
 </script>
+
+<style>
+    header {
+        height: 100vh;
+        width: 100%;        
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    header .header-image {
+        height: 95vh;
+    }
+</style>
